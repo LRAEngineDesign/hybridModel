b7028af1138a88081993ca591b7ce26f19655c07
nevangelista545 <nkme@utexas.edu>
2024-02-11 15:05:22 -0600

    made nitrous model cleaner, new PYTHON file for transient engine model

--- Nitrous/NitrousBlowdown.m
+++ Nitrous/NitrousBlowdown.m
@@ -12,13 +12,21 @@ initialTemperature = 293;   % K
 totalMass = 40.04;  % kg
 oxFlowRate = 2; % kg/s
 
-% mass [liquid & vapor mass(kg), mass vaporized(kg)]
-% state [ullage(%), temp(K), pressure(kPa)]
-% volLG [liquid & vapor volume(m^3)]
 t = linspace(0, tmax, tmax/dt);
-mass = zeros(3, length(t));
-state = zeros(3, length(t));
-volLG = zeros(2, length(t));
+
+% masses of system: liquid & vapor mass(kg), mass vaporized(kg)
+massLiq = zeros(1, length(t)); % kg
+massVapor = zeros(1, length(t)); % kg
+vaporizedMass = zeros(1, length(t)); % kg
+
+% state of system: ullage(%), temp(K), pressure(kPa)
+ullage = zeros(1, length(t)); % percent
+temp = zeros(1, length(t)); % K
+pressure = zeros(1, length(t)); % kPa
+
+% volLG [liquid & vapor volume(m^3)]
+volLiq = zeros(1, length(t));
+volVapor = zeros(1, length(t));
 
 %% Initial Conditions
 % Using iniTemp, uses thermodynamic properties at that state to calculate
@@ -27,10 +35,16 @@ volLG = zeros(2, length(t));
 
 % N2O [liqDensity, vapDensity, vapPressure, hVap, liqSpecHeat, vapSpecHeat]
 N2O = NitrousProperties(initialTemperature);
-mass(1) = (totalMass-volTank*N2O(2)) / (1-N2O(2)/N2O(1));
-mass(2) = totalMass - mass(1);
-state(:,1) = [(mass(2) / (N2O(2)*volTank)), initialTemperature, N2O(3)];
-volLG(:,1) = [mass(1)/N2O(1), mass(2)/N2O(2)];
+
+% initial conditions
+massLiq(1) = (totalMass-volTank*N2O(2)) / (1-N2O(2)/N2O(1));
+vapMass(1) = totalMass - massLiq(1);
+ullage(1) = vapMass(1) / (N2O(2)*volTank);
+temp(1) = initialTemperature;
+pressure(1) = N2O(3);
+
+volLiq(1) = massLiq(1)/N2O(1);
+volVapor(1) = massVapor(1)/N2O(2);
 
 %% simulation @ t = 0
 % when ox leaves tank, some empty volume is left, so some of the remaining
@@ -41,15 +55,17 @@ volLG(:,1) = [mass(1)/N2O(1), mass(2)/N2O(2)];
 
 for n = 2:length(t)
     volumeEmptied = oxFlowRate*dt / N2O(1);
-    mass(3,n) = volumeEmptied * N2O(2);
-    state(2,n) = state(2,n-1) - (N2O(4)*mass(3,n-1) / (mass(1,n-1)*N2O(5) + mass(2,n-1)*N2O(6)));
-    N2O = NitrousProperties(state(2,n));
-    volLG(:,n) = [(volLG(1,n-1)-volumeEmptied), (volLG(2,n-1)+volumeEmptied)];
-    mass(1:2,n) = [volLG(1,n)*N2O(1), volLG(2,n)*N2O(2)];
-    state(1,n) = mass(2,n) / (N2O(2)*volTank);
-    state(3,n) = N2O(3);
+    vaporizedMass(1,n) = volumeEmptied * N2O(2);
+    temp(n) = temp(n-1) - (N2O(4)*vaporizedMass(n-1) / (massLiq(n-1)*N2O(5) + massVapor(n-1)*N2O(6)));
+    N2O = NitrousProperties(temp(n));
+    volLiq(n) = volLiq(n-1) - volumeEmptied;
+    volVapor(n) = volVapor(n-1) + volumeEmptied;
+    massLiq(n) = volLiq(n)*N2O(1);
+    massVapor(n) = volVapor(n)*N2O(2);
+    ullage(n) = massVapor(n) / (N2O(2)*volTank);
+    pressure(n) = N2O(3);
     
-    if (state(1,n) >= 1) || (mass(1,n) <= 0)
+    if (ullage(n) >= 1) || (massLiq(n) <= 0)
         vaporPhaseTime = n;
         break
     end
@@ -57,19 +73,19 @@ end
 
 figure;
 subplot(221)
-plot(t(1:vaporPhaseTime), state(3,1:vaporPhaseTime))
+plot(t(1:vaporPhaseTime), pressure(1:vaporPhaseTime))
 xlabel('Time(seconds)')
 ylabel('Pressure(kPa)')
 title('Pressure Drop of Nitrous Tank')
 
 subplot(222)
-plot(t(1:vaporPhaseTime), state(2,1:vaporPhaseTime))
+plot(t(1:vaporPhaseTime), temp(1:vaporPhaseTime))
 xlabel('Time(seconds)')
 ylabel('Temperature(K)')
 title('Temperature Drop of Nitrous Tank')
 
 subplot(223)
-plot(t(1:vaporPhaseTime), mass(1,1:vaporPhaseTime), t(1:vaporPhaseTime), mass(2,1:vaporPhaseTime))
+plot(t(1:vaporPhaseTime), massLiq(1:vaporPhaseTime), t(1:vaporPhaseTime), massVapor(1:vaporPhaseTime))
 xlabel('Time(seconds)')
 ylabel('Mass(kg)')
 title('Nitrous Mass in Tank')
