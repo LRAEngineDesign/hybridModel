3dfe264ddc13b399d8e1f953be5fd5be3c173cbc
Isaac <isaacbsal@gmail.com>
2024-02-11 20:01:17 -0600

    Worked on combustion. AGH

---
+++ PYTHON_Engine_Model/combustion.py
@@ -0,0 +1,6 @@
+def combustion(dt, OFRatio):
+    #do stuff
+    # Probably use nasa CEA stuff to make curves we can reference
+    chamberpressure = 7
+    temperature = 7
+    return [chamberPressure, temperature]
