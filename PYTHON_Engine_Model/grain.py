451b07bd4ebc1ce4e3c6daf969af66359c9ea43e
Isaac <isaacbsal@gmail.com>
2024-02-11 19:55:31 -0600

    Added combustion function

--- PYTHON_Engine_Model/grain.py
+++ PYTHON_Engine_Model/grain.py
@@ -1,16 +1,30 @@
 import math
 
-def grain(dt, oxMdot, ID, length):
-    oxflux = oxMdot * math.pi * length * ID #kg s^-1 m^-2
+def grain(dt, oxMdot, ID, length): # Use SI units pleaseeee
+    oxflux = oxMdot/(math.pow(ID,2) * math.pi * 0.25) #kg s^-1 m^-2
     ### Constants
     a = 0.417 #constants are based on mm/s and g/cm^2
     n = 0.347
-    fuelDensity = 950 #kg/m^3
+    fuelDensity = 950 #kg m^-3
 
-    regressionRate = 1000 * a * math.pow(oxflux*0.1, n) #m s^-1 
-    fMdot = ((regressionRate * dt + ID*0.5)^2 - (ID*0.5)^2) * math.pi * 0.5 * length * fuelDensity # Fuel mass flow rate 
-        
-    OFRatio = oxMdot/fMdot
-    ID = regressionRate * dt + ID*0.5
-
+    regressionRate = 0.001 * a * math.pow(oxflux*0.1, n) #m s^-1, radius regression rate. Const
+    fMdot = ((math.pow((regressionRate * dt + ID*0.5),2) - math.pow(ID*0.5, 2)) * math.pi * 0.5 * length * fuelDensity)/dt # Fuel mass flow rate 
+    OFRatio = oxMdot/fMdot # no dimension
+    ID = 2 * regressionRate * dt + ID
     return [ID, OFRatio]
+
+'''
+#This is here for sanity checking
+dt = 0.001
+t = 0
+oxMdot = 1
+ID = 0.03
+OD = 0.08
+length = 0.5
+while(ID < OD):
+    [ID, OFRatio] = grain(dt, oxMdot, ID, length)
+    t += dt
+
+print(t)
+print(OFRatio)
+'''
