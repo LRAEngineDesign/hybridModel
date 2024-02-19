ab028f18072f34caf08f4e0d5c2c2a3ce55b94f6
Kyle <nkme@utexas.edu>
2024-02-19 02:21:24 -0600

    input configuration files and more progress on nitrous stuff

--- PYTHON_Engine_Model/NitrousBlowdown.py
+++ PYTHON_Engine_Model/NitrousBlowdown.py
@@ -1,38 +1,71 @@
 import numpy as np
 import matplotlib as plot
+import yaml
+
+# Probably dont need a yaml for nitrous coefficients
+#with open('nitrous_coefficients.yaml') as file1:
+#    nitrous = yaml.safe_load(file1)
+blowdown_inputs = "C:/Users/Kyle/OneDrive/Documents/MATLAB/hybridModel/PYTHON_Engine_Model/blowdown_inputs.yaml"
+
+with open(blowdown_inputs, 'r') as file2:
+        inputs = yaml.safe_load(file2)
+
 
 # Thermophysical Properties of Nitrous Oxide White Paper
 # Range of applicability: -90C to 36C (-130F to 96.8F)
-# Can only model based on temperature (eqns based off temp since
-# using the saturation dome and too complex)
+# Can only model based on temperature (eqns based off temp since using the saturation dome and too complex)
 
-# Critical Point of Nitrous
-critTemp = 309.57   # K
-critRho = 452       # kg/m**3
-critPressure = 7251 # kPa
+class Blowdown:
+    def __init__(self, abs_temp):
 
-# ---------- Polynomial Constants from Paper ----------
-vapPressure = [-6.7189, 1.35966, -1.3779, -4.051]       # vapor pressure (kPa)
-rhoL = [1.72328, -0.83950, 0.51060, -0.10412]           # liquid density (kg/m**3)
-rhoV = [-1.009, -6.28792, 7.50332, -7.90463, 0.629427]  # vapor density (kg/m**3)
+        self.absTemp = abs_temp
 
-hL = [-200, 116.043,-917.225, 794.779, -589.587]        # liquid spec. enthalpy (kJ/kg)
-hV = [-200, 440.055, -459.701, 434.081, -485.338]       # vapor spec, enthalpy (kJ/kg)
+        # --- Critical Point of Nitrous ---
+        self.critTemp = 309.57   # K
+        self.critRho = 452       # kg/m^3
+        self.critPressure = 7251 # kPa
 
-cL = [2.49973, 0.023454, -3.80136, 13.0945, -14.5180]   # liquid isobaric spec. heat capacity (kJ/kg*K)
-cV = [132.632, 0.052187, -0.364923, -1.20233, 0.536141] # vapor isobaric spec. heat capacity (kJ/kg*K)
+        # ---------- Polynomial Constants from Paper ----------
+        self.vapPressure = [-6.7189, 1.35966, -1.3779, -4.051]       # vapor pressure (kPa)
+        self.rhoL = [1.72328, -0.83950, 0.51060, -0.10412]           # liquid density (kg/m^3)
+        self.rhoV = [-1.009, -6.28792, 7.50332, -7.90463, 0.629427]  # vapor density (kg/m^3)
 
-# ---------- Property Calculation from White Paper ----------
-def N2O_Properties(absTemp):
-    Tr = absTemp/critTemp
-    vaporPressure = critTemp*np.exp((1/Tr)*(vapPressure[0]*(1-Tr) + vapPressure[1]*(1-Tr)**(3/2) + vapPressure[2]*(1-Tr)**(5/2) + vapPressure[3]*(1-Tr)**5))
-    liquidDensity = critRho*np.exp(rhoL[0]*(1-Tr)**(1/3) + rhoL[1]*(1-Tr)**(2/3) + rhoL[2]*(1-Tr) + rhoL[3]*(1-Tr)**(4/3))
-    vaporDensity = critRho*np.exp(rhoV[0]*(1/Tr-1)**(1/3) + rhoV[1]*(1/Tr-1)**(2/3) + rhoV[2]*(1/Tr-1) + rhoV[3]*(1/Tr-1)**(4/3) + rhoV[4]*(1/Tr-1)**(5/3))
-    hVaporization = (hV[0]-hL[0]) + (hV[1]-hL[1])*(1-Tr)**(1/3) + (hV[2]-hL[2])*(1-Tr)**(2/3) + (hV[3]-hL[3])*(1-Tr) + (hV[4]-hL[4])*(1-Tr)**(4/3)
-    liquidSpecHeat = cL[0]*(1 + cL[1]*(1-Tr)**(-1) + cL[2]*(1-Tr) + cL[3]*(1-Tr)**2 + cL[4]*(1-Tr)**3)
-    vaporSpecHeat = cV[0]*(1 + cV[1]*(1-Tr)**(-2/3) + cV[2]*(1-Tr)**(-1/3) + cV[3]*(1-Tr)**(1/3) + cV[4]*(1-Tr)**(2/3))
+        self.hL = [-200, 116.043,-917.225, 794.779, -589.587]        # liquid spec. enthalpy (kJ/kg)
+        self.hV = [-200, 440.055, -459.701, 434.081, -485.338]       # vapor spec, enthalpy (kJ/kg)
 
-    return(liquidDensity, vaporDensity, vaporPressure/6.89475, hVaporization, liquidSpecHeat, vaporSpecHeat)
+        self.cL = [2.49973, 0.023454, -3.80136, 13.0945, -14.5180]   # liquid isobaric spec. heat capacity (kJ/kg*K)
+        self.cV = [132.632, 0.052187, -0.364923, -1.20233, 0.536141] # vapor isobaric spec. heat capacity (kJ/kg*K)
 
-n2o = N2O_Properties(100)
-print(n2o[1])
+        # ---------- Property Calculation from White Paper ----------
+    def N2O_Properties(self):
+        Tr = self.absTemp/self.critTemp
+        self.vaporPressure = self.critTemp*np.exp((1/Tr)*(self.vapPressure[0]*(1-Tr) + self.vapPressure[1]*(1-Tr)**(3/2) + self.vapPressure[2]*(1-Tr)**(5/2) + self.vapPressure[3]*(1-Tr)**5))
+        self.liquidDensity = self.critRho*np.exp(self.rhoL[0]*(1-Tr)**(1/3) + self.rhoL[1]*(1-Tr)**(2/3) + self.rhoL[2]*(1-Tr) + self.rhoL[3]*(1-Tr)**(4/3))
+        self.vaporDensity = self.critRho*np.exp(self.rhoV[0]*(1/Tr-1)**(1/3) + self.rhoV[1]*(1/Tr-1)**(2/3) + self.rhoV[2]*(1/Tr-1) + self.rhoV[3]*(1/Tr-1)**(4/3) + self.rhoV[4]*(1/Tr-1)**(5/3))
+        self.hVaporization = (self.hV[0]-self.hL[0]) + (self.hV[1]-self.hL[1])*(1-Tr)**(1/3) + (self.hV[2]-self.hL[2])*(1-Tr)**(2/3) + (self.hV[3]-self.hL[3])*(1-Tr) + (self.hV[4]-self.hL[4])*(1-Tr)**(4/3)
+        self.liquidSpecHeat = self.cL[0]*(1 + self.cL[1]*(1-Tr)**(-1) + self.cL[2]*(1-Tr) + self.cL[3]*(1-Tr)**2 + self.cL[4]*(1-Tr)**3)
+        self.vaporSpecHeat = self.cV[0]*(1 + self.cV[1]*(1-Tr)**(-2/3) + self.cV[2]*(1-Tr)**(-1/3) + self.cV[3]*(1-Tr)**(1/3) + self.cV[4]*(1-Tr)**(2/3))
+
+        # sanity checking
+        print(self.liquidDensity, self.vaporDensity, self.vaporPressure/6.89475, self.hVaporization, self.liquidSpecHeat, self.vaporSpecHeat)
+        print(type('this is working'))
+    
+
+    # ---------- Tank Discharge Calculations ----------
+
+    # Using initial temperature, uses thermodynamic properties at that state to calculate
+    # the starting mass of both liquid and vapor based on Law of Conservation of Mass and Volume
+
+    # --- Initial Conditions and Initializing State Variables ---
+
+    # random parameters for now
+    volTank = inputs['Tank_Volume']                     # m^3
+    initialTemperature = inputs['Initial_Temperature']  # K
+    oxMass = inputs['Ox_Mass']                          # kg
+    oxFlowRate = 2                                      # kg/s
+
+
+# sanity checking
+stuff = Blowdown(293)
+stuff.N2O_Properties()
+print(stuff.critPressure)
